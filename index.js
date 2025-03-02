149204259d8f3aa31436f871595f914682cffdc9
Makuer <makuer.mqtur@student.moringaschool.com>
2025-03-02 12:14:30 +0300

    loops

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(name,eventName){
+    const message = [];
+    for(let i = 0; i<name.length;i++){
+        message.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`);
+    }
+
+    return message;
+}
+
+function countDown(name){
+    for(let i = name; i >=0; i--){
+        console.log(i);
+    }
+}
